583f256bbb102ad0daf51e91ef4775e4b51d22c8
AppVeyor <ci@appveyor.com>
2020-08-02 14:16:51 +0000

    4e21fcf

---
+++ precache-manifest.bd79a3e76df2344d20b361be90020e8c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2152d2c9bf7f58cff7f85109acd9eae9",
+    "url": "/weather-app-redux/index.html"
+  },
+  {
+    "revision": "e5c54b12441b34c9ed30",
+    "url": "/weather-app-redux/static/js/2.62d5d830.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/weather-app-redux/static/js/2.62d5d830.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "08181268f37e76b2e426",
+    "url": "/weather-app-redux/static/js/main.5cd1ad83.chunk.js"
+  },
+  {
+    "revision": "7b22fb306a42dd54c19a",
+    "url": "/weather-app-redux/static/js/runtime-main.ec729aa8.js"
+  }
+]);
